5283cb5eeb7335f43eb6cc5950b886d7681d1c22
scarrrr316 <43269183+scarrrr316@users.noreply.github.com>
2020-04-13 15:26:41 +0800

    Update Run.bat

--- InteractiveHtmlBom/Run.bat
+++ InteractiveHtmlBom/Run.bat
@@ -1,26 +1,28 @@
 @echo off
-
+set pathofEDASourceFile=%1
 echo -------------------------------------------------------------------------------------------------------------------
 echo -------------------------------------------------------------------------------------------------------------------
 echo                                                                                                                                                                                    -
 echo                                  Thank you for using InteractiveHtmlBom
-echo                           https://github.com/openscopeproject/InteractiveHtmlBom
+echo                           httpsgithub.comopenscopeprojectInteractiveHtmlBom
 echo                                         Bat file by Scarrrr0725
 echo                                                                                                                                                                                    -
 echo --------------------------------------------------------------------------------------------------------------------
 echo --------------------------------------------------------------------------------------------------------------------
 
-::delete --show-dialog after frist start up and setting
+delete --show-dialog after frist start up and setting
 set option=--show-dialog
 
 set FilePath=%~dp0
 set pyFilePath=%FilePath%generate_interactive_bom.py
 
-:convert
-set /p pathofEDASourceFile=Please Drag the EasyEDA PCB source file here :
-
+convert
+if not defined pathofEDASourceFile (
+	set p pathofEDASourceFile=Please Drag the EasyEDA PCB source file here 
+) 
 echo  Converting. . . . . . . . . .
 python %pyFilePath% %pathofEDASourceFile% %option%
+set pathofEDASourceFile=
 
 echo -------------------------------------------------------------------------------------------------------------------
 echo -------------------------------------------------------------------------------------------------------------------
@@ -30,6 +32,6 @@ echo                                                                            
 echo -------------------------------------------------------------------------------------------------------------------
 echo -------------------------------------------------------------------------------------------------------------------
 
-CHOICE /C YN /N /M "Do you want to convert another file? [Y/N"
+CHOICE C YN N M Do you want to convert another file [YN]
 	if errorlevel 2 exit
 	if errorlevel 1 goto convert
